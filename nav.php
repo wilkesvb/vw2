98d2927c3bcb1cf793fa00f6d7b6223cf7e4da9e
Vincent Wilkes <wilkesvb@gmail.com>
2013-02-25 22:52:13 -0500

    Nav edit

---
+++ nav.php
@@ -0,0 +1,5 @@
+<nav class="main-nav">
+  <a class="icon-home" href="vincentwilkes.com"><span class="screen-reader-text">Home</span></a>
+  <a class="icon-user" href="/about/"><span class="screen-reader-text">About</span></a>
+  <a class="icon-archive" href="/archives/"><span class="screen-reader-text">Archive</span></a>
+</nav>
